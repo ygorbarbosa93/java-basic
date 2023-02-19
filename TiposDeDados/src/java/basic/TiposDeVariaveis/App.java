b9f518b515b1aaf7140a52080fd8683410491919
Ygor Barbosa <ygor.s.barbosa@gmail.com>
2023-02-19 14:19:19 -0300

    introdução a tipos de variaveis

---
+++ TiposDeDados/src/java/basic/TiposDeVariaveis/App.java
@@ -0,0 +1,11 @@
+package java.basic.TiposDeVariaveis;
+public class App {
+    public static void main(String[] args) throws Exception {
+        int numero = 10;
+        float decimalCurto = 10.5f;
+        double decimalLongo = 20.5;
+        char caracter = 'A';
+        String cadeiaCaracteres = "Curso de Java Básico";
+        boolean certoOuErrado = true;
+    }
+}
